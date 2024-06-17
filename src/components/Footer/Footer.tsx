fb3a7bce18c483c61e33633271361ffb1b357257
sebastianbanagasta <sebastian.09.10.1995@gmail.com>
2024-06-17 01:02:46 -0500

    Mobile First con Tailwind

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,65 +3,65 @@
 export const Footer = () => {
     return (
         <>
-            <footer className='min-h-64 bg-[#7525a7] flex flex-col items-center '>
-                <div id="footer" className="w-[1080px] flex justify-between p-3 ">
-                    <ul className="w-44 list-none p-3">
+            <footer className='bg-[#7525a7] flex flex-col items-center pt-5'>
+                <div id="footer" className="w-full md:grid md:grid-cols-2 lg:grid-cols-5 lg:w-[1080px]">
+                    <ul className="w-full flex-col text-center list-none p-3">
                         <li className="font-bold pb-5">
-                            <a className="text-white no-underline" href="./index.html">Ofertas</a>
+                            <a className="text-white text-3xl md:text-2xl lg:text-xl no-underline" href="./index.html">Ofertas</a>
                         </li>
                         <li className="py-2 px-0">
-                            <a className="text-white no-underline" href="./index.html">Laptops</a>
+                            <a className="text-white text-xl md:text-base no-underline" href="./index.html">Laptops</a>
                         </li>
                         <li className="py-2 px-0">
-                            <a className="text-white no-underline" href="./index.html">Audio</a>
+                            <a className="text-white text-xl md:text-base no-underline" href="./index.html">Audio</a>
                         </li>
                         <li className="py-2 px-0">
-                            <a className="text-white no-underline" href="./index.html">Auriculares</a>
+                            <a className="text-white text-xl md:text-base no-underline" href="./index.html">Auriculares</a>
                         </li>
                     </ul>
-                    <ul className="w-[170px] list-none p-3">
+                    <ul className="w-full flex-col text-center list-none p-3">
                         <li className="font-bold pb-5">
-                            <a className="text-white no-underline" href="./index.html">Cómo comprar</a>
+                            <a className="text-white text-3xl md:text-2xl lg:text-xl no-underline" href="./index.html">Cómo comprar</a>
                         </li>
                         <li className="py-2 px-0">
-                            <a className="text-white no-underline" href="./index.html">Formas de pago</a>
+                            <a className="text-white text-xl md:text-base no-underline" href="./index.html">Formas de pago</a>
                         </li>
                         <li className="py-2 px-0">
-                            <a className="text-white no-underline" href="./index.html">Envios</a>
+                            <a className="text-white text-xl md:text-base no-underline" href="./index.html">Envios</a>
                         </li>
                         <li className="py-2 px-0">
-                            <a className="text-white no-underline" href="./index.html">Devoluciones</a>
+                            <a className="text-white text-xl md:text-base no-underline" href="./index.html">Devoluciones</a>
                         </li>
                     </ul>
-                    <ul className="w-[170px] list-none p-3">
+                    <ul className="w-full flex-col text-center list-none p-3">
                         <li className="font-bold pb-5">
-                            <a className="text-white no-underline" href="./index.html">Costos y tarifas</a>
+                            <a className="text-white text-3xl md:text-2xl lg:text-xl no-underline" href="./index.html">Costos y tarifas</a>
                         </li>
                         <li className="py-2 px-0">
-                            <a className="text-white no-underline" href="./index.html">Impuestos</a>
+                            <a className="text-white text-xl md:text-base no-underline" href="./index.html">Impuestos</a>
                         </li>
                         <li className="py-2 px-0">
-                            <a className="text-white no-underline" href="./index.html">Facturación</a>
+                            <a className="text-white text-xl md:text-base no-underline" href="./index.html">Facturación</a>
                         </li>
                     </ul>
-                    <ul className="w-[170px] list-none p-3">
+                    <ul className="w-full flex-col text-center list-none p-3">
                         <li className="font-bold pb-5">
-                            <a className="text-white no-underline" href="./index.html">Mis pedidos</a>
+                            <a className="text-white text-3xl md:text-2xl lg:text-xl no-underline" href="./index.html">Mis pedidos</a>
                         </li>
                         <li className="py-2 px-0">
-                            <a className="text-white no-underline" href="./index.html">Pedir nuevamente</a>
+                            <a className="text-white text-xl md:text-base no-underline" href="./index.html">Pedir nuevamente</a>
                         </li>
                         <li className="py-2 px-0">
-                            <a className="text-white no-underline" href="./index.html">Lista de deseos</a>
+                            <a className="text-white text-xl md:text-base no-underline" href="./index.html">Lista de deseos</a>
                         </li>
                     </ul>
-                    <ul className="w-[170px] list-none p-3">
+                    <ul className="w-full flex-col text-center list-none p-3">
                         <li className="font-bold pb-5">
-                            <a className="text-white no-underline" href="./index.html">Garantía</a>
+                            <a className="text-white text-3xl md:text-2xl lg:text-xl no-underline" href="./index.html">Garantía</a>
                         </li>
                     </ul>
                 </div>
-                <div className="w-[1080px] text-center text-white font-bold py-6 px-0 border-t border-solid border-[#ba53fb]">Curso de HTML + CSS 2023</div>
+                <div className="w-full md:w-[80%] lg:w-[1080px] text-center text-white font-bold py-6 px-0 border-t border-solid border-[#ba53fb]">Curso de HTML + CSS 2023</div>
             </footer>
         </>
     )
--- src/components/NavButton/NavButton.tsx
+++ src/components/NavButton/NavButton.tsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom'
 
-import styles from './NavButton.module.css'
+//import styles from './NavButton.module.css'
 
 export const NavButton = ({titleNav,linkNav}) => {
   return (
     <>
-        <li className={styles["nav-li"]}>
-            <Link className={styles["nav-a"]} to={linkNav}>{titleNav}</Link>
+        <li className="list-none text-center p-3">
+            <Link className="no-underline font-bold text-white" to={linkNav}>{titleNav}</Link>
         </li>
     </>
   )
